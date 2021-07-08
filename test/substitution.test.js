bce6228deffe50333ffe7098408bf4597fd4ae00
Sean Glass <scglass13@gmail.com>
2021-07-07 20:09:15 -0500

    added pseudocode

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -3,18 +3,21 @@ const substitutionModule = require("../src/substitution");
 
 describe("Substitution", () => {
     
+//     checks to see if there is any input
     it("Should return false if input was not provided", () => {
         const expected = false;
         const actual = substitutionModule.substitution("message");
         expect(actual).to.equal(expected);
       });
 
+//     test to see if alphabet was provided
     it("Should return false if alphabet was not provided", () => {
         const expected = false;
         const actual = substitutionModule.substitution("message");
         expect(actual).to.equal(expected);
       });
 
+//     check the number of characters in the alphabet
     it("Should return false if alphabet is not exactly 26 characters long", () => {
       const expected = false;
       const actual = substitutionModule.substitution("message", "plmoknijbuhvygctfxrdzeswaqaaaaa");
@@ -27,18 +30,21 @@ describe("Substitution", () => {
         expect(actual).to.equal(expected);
     });
 
+//     check for duplicate letters in the alphabet
     it("should return false if duplicate values found in the given alphabet", () => {
         const expected = false;
         const actual = substitutionModule.substitution("message", "abcdefghijklmnopqrstuvwxyy");
         expect(actual).to.equal(expected);
     });
-
+    
+//      check for spacing consistency
     it("Encoding: should maintain spaces in the message", () => {
         const expected = "elp xhm xf mbymwwmfj dne";
         const actual = substitutionModule.substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
         expect(actual).to.equal(expected);
     });
 
+//     message is decoding
     it("Decoding", () => {
         const expected = "message";
         const actual = substitutionModule.substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
